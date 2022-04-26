f4ab037d24b7e6cfd7d936fc8e1054c0e9bc7f9a
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2022-04-26 15:05:27 +0200

    ed

--- app.js
+++ app.js
@@ -36,13 +36,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //coden som linker databasen tverrDB til koden 
-mongoose.connect("mongodb://localhost:27017/tverrDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/tverrDB", {
+    useNewUrlParser: true
+});
 
 //
-const userSchema = new mongoose.Schema ({
-    email: String,  // email
+const userSchema = new mongoose.Schema({
+    email: String, // email
     password: String,
-    googleId: String,  // dette er lagrer google IDen hvis du loger in med google
+    googleId: String, // dette er lagrer google IDen hvis du loger in med google
     facebookId: String, // dette er lagrer facebook IDen hvis du loger in med facebook
     secret: String
 });
@@ -55,12 +57,12 @@ const User = new mongoose.model("tverruser", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
         done(err, user);
     })
 });
@@ -68,51 +70,59 @@ passport.deserializeUser(function(id, done) {
 
 //google strategy
 passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/tverrfag", 
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/tverrfag",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
 
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+        User.findOrCreate({
+            googleId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
 ));
 
 
 // facebook strategy
 passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_CLIENT_ID,
-    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets", 
+        clientID: process.env.FACEBOOK_CLIENT_ID,
+        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/facebook/secrets",
 
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
 
-    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
+        User.findOrCreate({
+            facebookId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
 ));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.render("home");
 });
 
 
 //auth google   dette sjekker om du du er tillat av google til bli athenticated
 app.get("/auth/google",
-    passport.authenticate("google", { scope: ["profile"] })
+    passport.authenticate("google", {
+        scope: ["profile"]
+    })
 );
 
-app.get("/auth/google/tverrfag", 
-    passport.authenticate("google", { failureRedirect: "/login"}),
-    function(req, res) {
+app.get("/auth/google/tverrfag",
+    passport.authenticate("google", {
+        failureRedirect: "/login"
+    }),
+    function (req, res) {
         res.redirect("/documentation")
     }
 );
@@ -120,43 +130,53 @@ app.get("/auth/google/tverrfag",
 
 //auth facebook      dette sjekker om du du er tillat av facebook til bli athenticated
 app.get("/auth/facebook",
-    passport.authenticate("facebook", { scope: ["profile"] })
+    passport.authenticate("facebook", {
+        scope: ["profile"]
+    })
 );
 
-app.get("/auth/facebook/tverrfag", 
-    passport.authenticate("facebook", { failureRedirect: "/login"}),
-    function(req, res) {
+app.get("/auth/facebook/tverrfag",
+    passport.authenticate("facebook", {
+        failureRedirect: "/login"
+    }),
+    function (req, res) {
         res.redirect("/Documentation")
     }
 );
 
 // render login
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
     res.render("login")
 });
 
 // render registrere
-app.get("/register", function(req, res) {
+app.get("/register", function (req, res) {
     res.render("register")
 });
 
 // render documentasjon HVIS den har funnet brukeren
-app.get("/documentation", function(req, res){
+app.get("/documentation", function (req, res) {
 
     //  
-    User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+    User.find({
+        "secret": {
+            $ne: null
+        }
+    }, function (err, foundUsers) {
         if (err) {
             console.log(err)
         } else {
             if (foundUsers) {
-                res.render("documentation", {usersWithSecrets: foundUsers})
+                res.render("documentation", {
+                    usersWithSecrets: foundUsers
+                })
             }
         }
     })
 });
 
 // hvis IsAthenticated = true så skal den render submit
-app.get("/submit", function(req, res) {
+app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
         res.render("submit")
     } else {
@@ -165,17 +185,17 @@ app.get("/submit", function(req, res) {
 })
 
 //
-app.post("/submit", function(req, res) {
+app.post("/submit", function (req, res) {
     const submittedSecrets = req.body.secret;
 
     console.log(req.user.id);
 
-    User.findById(req.user.id, function(err, foundUser) {
+    User.findById(req.user.id, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             foundUser.secret = submittedSecrets;
-            foundUser.save(function(){
+            foundUser.save(function () {
                 res.redirect("/documentation")
             })
         }
@@ -183,48 +203,50 @@ app.post("/submit", function(req, res) {
 })
 
 //hvis logout knappen eller skriver in /logout så skal den logout og gå til homepage
-app.get("/logout", function(req, res) {
+app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
 });
 
 // 
-app.post("/register", function(req, res) {
-    User.register({username: req.body.username}, req.body.password, function(err, user){
+app.post("/register", function (req, res) {
+    User.register({
+        username: req.body.username
+    }, req.body.password, function (err, user) {
         if (err) {
             console.log(err)
             res.redirect("/register")
         } else {
-            passport.authenticate("local")(req, res, function(){
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/")
             })
         }
     })
-})   
+})
 
 // 
-app.post("/login", function(req, res) {
+app.post("/login", function (req, res) {
 
     const user = new User({
         username: req.body.username,
         password: req.body.password
     })
 
-    req.login(user, function(err) {
+    req.login(user, function (err) {
         if (err) {
             console.log(err)
         } else {
-            passport.authenticate("local")(req, res, function(){
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/documentation")
             })
         }
     })
 
-}) 
+})
 
 
 
 // starter opp serveren på 3000
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log("Server started on port 3000")
 })
