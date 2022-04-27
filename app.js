1ebe79198a25fe11624245de3c715a85de8fca1a
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2022-04-27 18:47:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -95,24 +95,6 @@ passport.use(new GoogleStrategy({
 ));
 
 
-// facebook strategy
-passport.use(new FacebookStrategy({
-        clientID: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/facebook/secrets",
-
-    },
-    function (accessToken, refreshToken, profile, cb) {
-        console.log(profile);
-
-        User.findOrCreate({
-            facebookId: profile.id
-        }, function (err, user) {
-            return cb(err, user);
-        });
-    }
-));
-
 app.get("/", function (req, res) {
     res.render("home");
 });
