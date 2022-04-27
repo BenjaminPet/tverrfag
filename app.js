5547e7464c372a6bfc4567c980eb31031fe25198
Hacker_Master01 <56979864+HackerMaster01@users.noreply.github.com>
2022-04-27 08:46:15 +0200

    heroku

--- app.js
+++ app.js
@@ -246,9 +246,12 @@ app.post("/login", function (req, res) {
 
 })
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
 
-
-// starter opp serveren på 3000
-app.listen(3000, function () {
-    console.log("Server started on port 3000")
+// starter opp serveren på nett
+app.listen(port, function () {
+    console.log("Server shas started successfully")
 })
